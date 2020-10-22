3450281876fcdb90c7465482ebb9dc902fd62d8d
Graham R Pugh <graham.pugh@id.ethz.ch>
2020-10-22 17:20:29 +0200

    fix references to matched_filepath

--- Processors/JamfComputerGroupUploader.py
+++ Processors/JamfComputerGroupUploader.py
@@ -231,6 +231,7 @@ class JamfComputerGroupUploader(Processor):
 
         # if not found, search RECIPE_SEARCH_DIRS to look for it
         search_dirs = self.env.get("RECIPE_SEARCH_DIRS")
+        matched_filepath = ""
         for d in search_dirs:
             for path in Path(d).rglob(filename):
                 matched_filepath = str(path)
