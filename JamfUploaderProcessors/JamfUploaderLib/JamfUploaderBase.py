7436b8b76a083f67c5688db22e219b342a98f45c
Graham Pugh <graham.pugh@jamf.com>
2025-02-17 13:33:39 +0100

    bump date

--- JamfUploaderProcessors/JamfUploaderLib/JamfUploaderBase.py
+++ JamfUploaderProcessors/JamfUploaderLib/JamfUploaderBase.py
@@ -43,7 +43,7 @@ class JamfUploaderBase(Processor):
     """Common functions used by at least two JamfUploader processors."""
 
     # Global version
-    __version__ = "2025.2.12.0"
+    __version__ = "2025.2.17.0"
 
     def api_endpoints(self, object_type):
         """Return the endpoint URL from the object type"""
